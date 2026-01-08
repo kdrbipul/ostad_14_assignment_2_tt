f26ca43680bedf1b34c6fb605064c8b73c45a916
kdrbipul <kdrbipul@gmail.com>
2026-01-08 17:06:59 +0600

    some correction

--- lib/widget/destination_card.dart
+++ lib/widget/destination_card.dart
@@ -11,55 +11,53 @@ class DestinationCard extends StatelessWidget {
     return SizedBox(
       height: 260,
       width: 390,
-      child: Expanded(
-        child: GridView.builder(
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 3,
-          ),
-          itemCount: cityItem.length - 1,
-          itemBuilder: (context, index) {
-            final city = cityItem.elementAt(index);
-            return Card(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10),
-              ),
-              color: Colors.green,
-              elevation: 3,
-              child: Stack(
-                children: [
-                  Image(
-                    image: AssetImage(city.image),
-                    fit: BoxFit.cover,
-                    height: double.infinity,
-                    width: double.infinity,
-                  ),
-                  Positioned(
-                    bottom: 0,
-                    left: 0,
-                    right: 0,
-                    child: Container(
-                      height: 30,
-                      width: 150,
-                      color: Colors.grey.shade400,
-                    ),
-                  ),
-                  Positioned(
-                    left: 10,
-                    bottom: 3,
-                    child: Text(
-                      city.name,
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            );
-          },
+      child: GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3,
         ),
+        itemCount: cityItem.length - 1,
+        itemBuilder: (context, index) {
+          final city = cityItem.elementAt(index);
+          return Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            color: Colors.green,
+            elevation: 3,
+            child: Stack(
+              children: [
+                Image(
+                  image: AssetImage(city.image),
+                  fit: BoxFit.cover,
+                  height: double.infinity,
+                  width: double.infinity,
+                ),
+                Positioned(
+                  bottom: 0,
+                  left: 0,
+                  right: 0,
+                  child: Container(
+                    height: 30,
+                    width: 150,
+                    color: Colors.grey.shade400,
+                  ),
+                ),
+                Positioned(
+                  left: 10,
+                  bottom: 3,
+                  child: Text(
+                    city.name,
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
       ),
     );
   }
